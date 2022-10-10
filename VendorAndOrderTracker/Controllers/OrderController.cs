f66d3b5a81fb5709fd9671ffcec973781d65e975
TrevorH08 <trevorh8700@gmail.com>
2022-10-10 07:19:32 -0700

    add get and post routes to Vendors Controller

---
+++ VendorAndOrderTracker/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace VendorAndOrderTracker.Controllers
+{
+  public class OrdersController : Controller
+  {
+
+  }
+}
