e8d3581c884da2fcdbed9b4ca746905594cc1c2e
TrevorH08 <trevorh8700@gmail.com>
2022-10-10 05:56:33 -0700

    add passing test for Vendor Constructor

--- VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -8,5 +8,17 @@ namespace VendorAndOrderTracker.Tests
   [TestClass]
   public class VendorTests : IDisposable
   {
+
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("Test Vendor");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
   }
 }
