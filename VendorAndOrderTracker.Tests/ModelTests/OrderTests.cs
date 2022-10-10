9f8e43aa2c1b9101b92ccb34b656dbc87af60452
TrevorH08 <trevorh8700@gmail.com>
2022-10-10 04:57:19 -0700

    add ClearAll to Order and add passing test for constructor

---
+++ VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendorAndOrderTracker.Models;
+
+namespace VendorAndOrderTracker.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test", "10/10/22", "test order", 500);
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}
